a83575cd78c699687573ebaa00f5936eabc3dbcb
Gerard Fevill <nouglozeh_gerard@yahoo.fr>
2021-05-06 03:01:27 +0200

    Ajout du formulaire Suppression

--- src/app/accueil/formulaires/suppression-form/suppression-form.component.ts
+++ src/app/accueil/formulaires/suppression-form/suppression-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClanService, PersonneService } from '../../../core/services';
 
 @Component({
   selector: 'app-suppression-form',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppressionFormComponent implements OnInit {
 
-  constructor() { }
+  path;
+  item;
+  items;
+  errors
+
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _clanService: ClanService,
+    private _personneService: PersonneService) { }
 
   ngOnInit(): void {
+    this._route
+    .data
+    .subscribe(d => {
+      this.path = history.state.path; 
+      this.item = history.state.itemData;
+    });
+  }
+
+  supprimer(){
+    switch (this.path) {
+      case 'clan':
+        this._clanService.deleteClan(this.item).subscribe(
+          result => {
+            this.items = result;
+          },
+          error => {
+            this.errors = error;
+          },
+          () => {
+            this._router.navigate(['accueil/' + this.path ]);
+          }
+        );
+        break;
+      case 'pers':
+        this._personneService.deletePersonne(this.item).subscribe(        
+          result => {
+            this.items = result;
+          },
+          error => {
+            this.errors = error;
+          },
+          () => {
+            this._router.navigate(['accueil/' + this.path ]);
+          }
+        );
+        break;
+      default:
+    }
+
+  }
+ 
+  annuler(){
+    this._router.navigate(['accueil/' + this.path ]);
   }
 
 }
