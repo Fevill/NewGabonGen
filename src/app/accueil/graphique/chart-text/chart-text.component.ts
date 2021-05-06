0a59a94fc9620127b2dc4edc4a1639fa2757d045
Gerard Fevill <nouglozeh_gerard@yahoo.fr>
2021-05-06 03:03:57 +0200

    Ajout du Chart text

--- src/app/accueil/graphique/chart-text/chart-text.component.ts
+++ src/app/accueil/graphique/chart-text/chart-text.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { NodeChart } from '../../../model/node-chart';
 
 @Component({
   selector: 'app-chart-text',
   templateUrl: './chart-text.component.html',
   styleUrls: ['./chart-text.component.scss']
 })
-export class ChartTextComponent implements OnInit {
+export class ChartTextComponent implements OnInit, OnChanges {
+
+  @Input() nodeChart: NodeChart;
+  @Input() initial: Boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  ngOnChanges(changes: SimpleChanges) {
+  }
+  
+  collapse(id){
+    return 'collapse'+id;
+  }
+
+  getCollapse(id){
+    return '#collapse'+id;
   }
 
 }
